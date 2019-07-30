5aa7c4eb25dbaf7ee6dca10b885340a31e511122
Lucas VASQUEZ DE OLIVEIRA - CEDRO <lucas.oliveira@tecka>
2019-07-30 08:45:22 -0300

    frist commit Lucas

---
+++ src/test/java/testes/InformacoesUsuarioTest.java
@@ -0,0 +1,40 @@
+package testes;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InformacoesUsuarioTest {
+    @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
+       //Abrindo o Navegador
+        System.setProperty("webdriver.chrome.driver", "C:\\Driver\\chromedriver.exe");
+        WebDriver navegador = new ChromeDriver();
+        navegador.manage().window().maximize();
+
+        //Navegando até a Página da SOC
+        navegador.get("https://ww2.soc.com.br/blog/");
+
+        //Identificando o formulário de Pesquisa
+        WebElement pesquisa = navegador.findElement(By.xpath("//input[@placeholder='Buscar']"));
+
+        //Digitar no campo com name "s" que está dentro do formulário de id "blog" o texto "teste"
+        pesquisa.sendKeys("teste");
+        pesquisa.submit();
+
+        //Validar que dentro do elemento com class "pagetitle search" está o texto "teste"
+        WebElement res = navegador.findElement(By.xpath("//*[@id='blog']/div/div[1]/div/h2/span"));
+        String textores = res.getText();
+        assertEquals("TESTE",textores);
+        // Fechar o Navegador
+        navegador.close();
+
+
+        //Validação
+        assertEquals(1,1);
+    }
+}
